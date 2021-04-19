dc39d85ec7b1239dc11e9dbfa9fa88197d53dacb
Bruno De Coene <brundeco@student.arteveldehs.be>
2021-04-19 17:24:30 +0200

    Add: contact form on contact page

--- resources/views/components/forms/contact.blade.php
+++ resources/views/components/forms/contact.blade.php
@@ -1,71 +1,31 @@
 <form id="form-contact" action="#" method="post" class="validate">
-  @include('components.forms.error')
-  @include('components.forms.success')
-
-  <div class="result"></div>
-
+  
   <div class="input-group">
-    <label for="name">Naam <span class="required">*</span></label>
-    <input type="text" name="name" id="name" required />
-  </div>
-  <div class="input-group">
-    <label for="email">E-mail <span class="required">*</span></label>
-    <input type="email" name="email" id="email" required />
-  </div>
-  <div class="input-group">
-    <label for="phone">Telefoon</label>
-    <input type="tel" name="phone" id="phone"/>
-  </div>
-  <div class="input-group">
-    <label for="select">Onderwerp</label>
+    <label for="select">Ik heb een vraag over</label>
     <select id="select" name="select">
       <option value="">Selecteer onderwerp</option>
-      <option value="Optie 1">Optie 1</option>
-      <option value="Optie 2">Optie 2</option>
+      <option value="Optie 1">Verkoop camper</option>
     </select>
   </div>
   <div class="input-group">
-    <label for="message">Bericht <span class="required">*</span></label>
-    <textarea name="message" id="message" required></textarea>
+    <label for="message">Je bericht of vraag <span class="required">*</span></label>
+    <textarea name="message" id="message" placeholder="Shoot!" required></textarea>
   </div>
 
-  <fieldset>
-    <legend>Kies een optie <span class="required">*</span></legend>
-    <div class="input-group input-group--horizontal">
-      <div class="radio">
-        <input type="radio" id="radio-1" name="radio" value="radio 1" required/>
-        <label for="radio-1">Optie 1</label>
-      </div>
-      <div class="radio">
-        <input type="radio" id="radio-2" name="radio" value="radio 2" required/>
-        <label for="radio-2">Optie 2</label>
+  <div class="grid">
+    <div class="grid__item one-half">
+      <div class="input-group">
+        <label for="name">Naam <span class="required">*</span></label>
+        <input type="text" name="name" id="name" placeholder="Naam" required />
       </div>
     </div>
-  </fieldset>
-
-  <fieldset>
-    <legend>Kies opties</legend>
-    <div class="input-group">
-      <div class="checkbox">
-        <input type="checkbox" id="checkbox-1" name="checkbox[]" value="checkbox 1" required/>
-        <label for="checkbox-1">Optie 1</label>
-      </div>
-      <div class="checkbox">
-        <input type="checkbox" id="checkbox-2" name="checkbox[]" value="checkbox 2" required/>
-        <label for="checkbox-2">Optie 2</label>
-      </div>
-    </div>
-  </fieldset>
-
-  <div class="input-group">
-    <div class="checkbox">
-      <input type="checkbox" id="privacy" name="privacy" value="1" required />
-      <label for="privacy">I agree my data will be processed in accordance with the <a href="privacy.html">privacy policy</a>. <span class="required">*</span></label>
+    <div class="grid__item one-half">
+      <div class="input-group">
+        <label for="email">E-mail <span class="required">*</span></label>
+        <input type="email" name="email" id="email" placeholder="Email" required />
+      </div>    
     </div>
   </div>
-
-  {{-- DEV recaptcha key --}}
-  <div class="g-recaptcha" data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" data-size="invisible"></div>
 
   <div class="button-group">
     <input type="submit" value="Verzenden" class="button"/>
--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -12,25 +12,47 @@ $breadcrumbs = [
 @section('content')
 <div class="section">
   <div class="container">
-
-    @include('components.base.breadcrumbs')
-
-    <div id="map" class="map"></div>
-
-    <h1>{{ $title }}</h1>
-
-    <div class="grid">
-      <div class="grid__item medium--one-half">
-        <h2>Adres</h2>
-      </div>
-      <div class="grid__item medium--one-half">
-        <h2>Contacteer ons</h2>
-
-        @include('components.forms.contact')
-
+    <div class="grid grid--justify-center">
+      <div class="grid__item three-fifths">
+        <div class="contact">
+          <div class="contact__top-section">
+            <h2 class="main-title contact-top-section__title">Neem contact met ons op </h2>
+            <p class="contact-top-section__text">Heb je een vraag? Check zeker onze FAQ. Vind je niet meteen een
+              antwoord of is je vraag specifiek? Stuur
+              ons gerust een mailtje of bel ons en wij nemen snel contact met je op.</p>
+            <div class="grid">
+              <div class="grid__item one-half">
+                <button class="main-button">
+                  +32 (0)495 99 68 90
+                </button>
+              </div>
+              <div class="grid__item one-half">
+                <button class="main-button">
+                  q-camper@outlook.be
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-
   </div>
 </div>
+
+
+<div class="section">
+  <div class="container">
+    <div class="grid grid--justify-center">
+      <div class="grid__item three-fifths">
+        <div class="contact__top-section">
+          <h2 class="main-title contact-top-section__title">Hoe kunnen we je helpen? </h2>
+          @include('components.forms.contact')
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="section"></div>
+
 @endsection
